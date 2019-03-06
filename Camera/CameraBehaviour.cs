b207235052664c017265707f3962e59b7abcf49c
davidguerreiro <me@davidguerreiro.com>
2019-03-06 00:07:06 +0000

    initial commit

---
+++ Camera/CameraBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    public float distanceFromPlayer = 0;
+    public float distanceFromTop = - 8f;
+    public float xAdjustement = - 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( player != null ) {
+            InitCameraPosition();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( player != null ) {
+            InitCameraPosition();
+        }
+    }
+
+    /// <summary>
+    /// Initializes camera position when the level starts
+    /// </summary>
+    private void InitCameraPosition() {
+        transform.position = new Vector3( player.transform.position.x - xAdjustement, player.transform.position.y - distanceFromTop, player.transform.position.z + distanceFromPlayer );
+    }
+}
---
+++ Player/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float initZ;                                // init Z axis position for player.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fixBallPosition();
+        resetRotation();
+    }
+
+    /// <summary>
+    /// Adjust the ball to keep falling in the same direction.
+    /// <summary>
+    public void fixBallPosition() {
+        transform.position = new Vector3( transform.position.x, transform.position.y, initZ );
+    }
+
+    /// <summary>
+    /// Reset the ball rotation. Rotate adds a force that we want
+    /// to avoid, so the ball does not rotate.
+    /// </summary>
+    private void resetRotation() {
+        Quaternion q = transform.rotation;
+        q.eulerAngles = new Vector3( 0, 0, 0 );
+        transform.rotation = q;
+    }
+}
