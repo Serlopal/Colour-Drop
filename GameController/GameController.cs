4f91cd914363180d3c5ef2a038e7e9c0060f0186
davidguerreiro <me@davidguerreiro.com>
2019-03-16 23:43:06 +0000

    complete color ui function for gamecontroller

--- GameController/GameController.cs
+++ GameController/GameController.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
 
-    public GameObject player;                          // Player GameObject.
+    public GameObject player;                                   // Player GameObject.
 
-    public Material[] colors = new Material[3];        // List of possible materials the player can swich to.
+    public Material[] colors = new Material[3];                 // List of possible materials the player can swich to.
+    private GameObject[] uiPlayerColors;                        // List of possible player colors in the UI.
+
+    private Vector3 active = new Vector3( 1f, 1f, 1f );         // Vector 3 to use with the active item in the player colors UI.
+    private Vector3 inactive = new Vector3( 0.5f, 0.5f, 0.5f ); // Vector3 to use with inactive items in the player colors UI.
 
     // Start is called before the first frame update
     void Start()
     {
+        uiPlayerColors = GameObject.FindGameObjectsWithTag( "uiplayercolor" );
         
     }
 
@@ -48,7 +54,28 @@ public class GameController : MonoBehaviour
                 } else {
                     playerRenderer.material = colors[i + 1];
                 }
+                // change active color in the UI.
+                if ( uiPlayerColors != null ) {
+                   SetUiPlayerColor( ColorUtility.ToHtmlStringRGB( playerRenderer.sharedMaterial.color) );
+                }
                 break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Set UI color to match player color when the player's color changes
+    /// </summary>
+    /// <param name="hexaColor">string - Player's material color value in hexadecimal string</param>
+    private void SetUiPlayerColor( string hexaColor ) {
+        Debug.Log( "called" );
+        
+        for( int j = 0; j < uiPlayerColors.Length; j++ ) {
+            RectTransform uiPlayerColorRectTransform = uiPlayerColors[j].GetComponent<RectTransform>();
+            uiPlayerColorRectTransform.localScale = inactive;
+
+            if ( ColorUtility.ToHtmlStringRGB( uiPlayerColors[j].GetComponent<Image>().color ) == hexaColor ) {
+                uiPlayerColorRectTransform.localScale = active;
             }
         }
     }
