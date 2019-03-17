dd442fcf52fa7c63e4f3b27f7d2319ff35e5a0a2
davidguerreiro <me@davidguerreiro.com>
2019-03-17 21:46:20 +0000

    add function to start playing level

---
+++ UI/startLevelButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startLevelButton : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject startButton;
+
+    /// <summary>
+    /// Start level enabling player gravity,
+    /// so the ball starts falling.
+    /// </summary>
+    public void startLevel() {
+        if ( player != null && startButton != null ) {
+            player.GetComponent<Rigidbody>().useGravity = true;
+            Destroy( startButton );
+        }
+    }
+}
