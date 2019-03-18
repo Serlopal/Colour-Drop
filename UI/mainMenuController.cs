3e4461f91db7770db77d70ccbd52a7df06187560
davidguerreiro <me@davidguerreiro.com>
2019-03-18 21:19:45 +0000

    complete main menu screen

---
+++ UI/mainMenuController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainMenuController : MonoBehaviour
+{
+    private GameController gameController;                      // GameController Script.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.Find( "GameController" ).GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Start game button controller.
+    /// </summary>
+    public void startGameButtonController() {
+        if ( gameController != null ) {
+            gameController.LoadScene( "Level1" );
+        }
+    }
+
+    /// <summary>
+    /// Start tutorial levels button controller.
+    /// </summary>
+    public void startTutorialGameController() {
+        if ( gameController != null ) {
+            gameController.LoadScene( "Level1" );
+        }
+    }
+
+    /// <summary>
+    /// Exit game button controller.
+    /// </summary>
+    public void ExitGameController() {
+        if ( gameController != null ) {
+            gameController.LoadScene( "companySplashScreen" );
+        }
+    }
+}
