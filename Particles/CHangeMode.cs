a32d7293922728e574572a151078ecd37f21ae82
davidguerreiro <me@davidguerreiro.com>
2019-03-31 19:34:23 +0200

    add change mode collider script

---
+++ Particles/CHangeMode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHangeMode : MonoBehaviour
+{
+    private GameController gameController;              // GameController script.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.Find( "GameController" ).GetComponent<GameController>();    
+    }
+
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if ( other.tag == "Player" && gameController != null ) {
+            // TODO: Call changeMode method here.
+        }
+    }
+}
